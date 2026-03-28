dce73eca37e37a71437c0e54afc0a62beafe0aa2
Parth Gautam <parth.gautam@aurigait.com>
2026-03-28 10:27:23 +0530

    dev_parth

--- a2z_core_erp/a2z_core_erp/doctype/general_settings/general_settings.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, aurigait and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("General Settings", {
-// 	refresh(frm) {
-
-// 	},
-// });
